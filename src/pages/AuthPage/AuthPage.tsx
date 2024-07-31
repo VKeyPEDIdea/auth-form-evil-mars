d1bbadd6cc4fb6ef1c26d1c46534497c19bfb0e4
Pavel Karyapkin <karyapkin@gmail.com>
2024-07-31 23:18:09 +0500

    add test web custom component

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+class HelloWorldComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+customElements.define('hello-world', HelloWorldComponent);
+
 const AuthPage = () => {
   return (
     <>
       <h1>Auth Page</h1>
       <p>Description text</p>
+      <hello-world>Hello</hello-world>
     </>
   );
 };
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,12 @@
 declare module '*.module.css';
+
+type Props = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLElement>,
+  HTMLElement
+>;
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'hello-world': Props;
+  }
+}
